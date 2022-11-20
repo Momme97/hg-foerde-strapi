6fdb461a3bbd9b3eb336c024dadf4d40ec4d11dc
Momme Ristow <momme.ristow@prodyna.com>
2022-11-20 18:09:51 +0100

    init

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+#Stop all running containers
+docker stop $(docker ps -aq)
 
+#Remove all containers
+docker rm $(docker ps -aq)
+
+#Remove all old images
+docker rmi $(docker images -q)
+
+docker-compose up -d
