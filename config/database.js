3e36a5870a1f257320a24ee1c9b8e3a2c52c9c86
Momme Ristow <momme.ristow@prodyna.com>
2022-11-27 00:58:32 +0100

    added gymnasium component

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
-  /*
+  
     connection: {
       client: 'sqlite',
       connection: {
         filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
       },
       useNullAsDefault: true, },
-      */
- 
+      
+ /*
     connection: {
       client: 'postgres',
       connection: {
@@ -21,6 +21,6 @@ module.exports = ({ env }) => ({
       },
       debug: false,
     },
-
+    */
 
 });
