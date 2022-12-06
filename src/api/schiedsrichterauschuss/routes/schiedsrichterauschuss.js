fcb478ccc2c4c6eb1a589fa563453bd931f513e9
Momme Ristow <momme.ristow@prodyna.com>
2022-12-06 18:25:59 +0100

    added referee comittee page

---
+++ src/api/schiedsrichterauschuss/routes/schiedsrichterauschuss.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * schiedsrichterauschuss router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::schiedsrichterauschuss.schiedsrichterauschuss');
---
+++ src/api/schiedsrichterauschuss/services/schiedsrichterauschuss.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * schiedsrichterauschuss service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::schiedsrichterauschuss.schiedsrichterauschuss');
