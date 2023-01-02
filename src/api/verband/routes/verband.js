5350984c556cf118d3a452c68371aeab73249c89
Momme Ristow <momme.ristow@prodyna.com>
2023-01-03 00:13:37 +0100

    added association page

---
+++ src/api/verband/routes/verband.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * verband router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::verband.verband');
---
+++ src/api/verband/services/verband.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * verband service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::verband.verband');
